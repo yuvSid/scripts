1de16bb039f3bba4af1e9782d9089137cc194a42
Yuriy Sidorov <syvwgl@gmail.com>
2023-05-05 21:20:45 +0400

    Changed wordle script to get more statistics

--- python/parseChatWordle.py
+++ python/parseChatWordle.py
@@ -18,7 +18,7 @@ for i in rawData['messages']:
 
     user = i['from_id']
     if user not in data:
-        data[user] = [0, 0, i['from']]
+        data[user] = [0, 0, i['from'], 0, 0]
 
     if not i['text_entities']:
         continue
@@ -26,16 +26,29 @@ for i in rawData['messages']:
     string = i['text_entities'][0]['text']
     words = string.split()
 
-    if words[0] != "Wordle" or words[2][0] == 'X':
+    resultInStr = str(words[2] if len(words) > 2 else '')
+    if words[0] != "Wordle" or not (resultInStr[0].isdigit() or words[2][0] == 'X'):
         continue
+    
+    tries = int(words[2][0]) if words[2][0] != 'X' else 7
+    data[user][3] = data[user][3] + 1
+    data[user][4] = data[user][4] + tries
+    
+    if words[2][0] != 'X':
+        data[user][0] = data[user][0] + 1
+        data[user][1] = data[user][1] + tries
 
-    data[user][0] = data[user][0] + 1
-    data[user][1] = data[user][1] + int(words[2][0])
-
+print("Without lost as 7 tries:")
 for item in data.items():
     user = item[1]
     res = user[1]/user[0]
     print(f'{user[2]:10} ==> median: {res:.4f} ==> solved: {user[0]}')
+
+print("\nWith lost as 7 tries:")
+for item in data.items():
+    user = item[1]
+    res = user[4]/user[3]
+    print(f'{user[2]:10} ==> median: {res:.4f} ==> solved: {user[3]}')
   
 # Closing file
 f.close()
