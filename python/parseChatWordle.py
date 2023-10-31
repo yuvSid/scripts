97608a42aaafa1b0ccdcb06767ae046fd0984131
Юра Сидоров <syvwgl@gmail.com>
2023-10-31 14:20:02 +0400

    statistics calclulation include unsolved days as 7 tries, sorted print out

--- python/parseChatWordle.py
+++ python/parseChatWordle.py
@@ -38,17 +38,41 @@ for i in rawData['messages']:
         data[user][0] = data[user][0] + 1
         data[user][1] = data[user][1] + tries
 
-print("Without lost as 7 tries:")
+max_solved = 0
+solvers = []
 for item in data.items():
-    user = item[1]
-    res = user[1]/user[0]
-    print(f'{user[2]:10} ==> median: {res:.4f} ==> solved: {user[0]}')
+    if item[1][0] > max_solved:
+        max_solved = item[1][0]
+        solvers = []
+    if item[1][0] == max_solved:
+        solvers.append(item[1][2])
 
-print("\nWith lost as 7 tries:")
+print("Without lost count:")
+res = []
 for item in data.items():
     user = item[1]
-    res = user[4]/user[3]
-    print(f'{user[2]:10} ==> median: {res:.4f} ==> solved: {user[3]}')
+    res.append({"res" : user[1]/user[0], "name" : user[2], "solved" : user[0]})
+res.sort(key=lambda x: x["res"])
+for each in res:
+    print(f'{each["name"]:10} ==> average: {each["res"]:.4f} ==> solved: {each["solved"]}')
+
+# res = []
+# print("\nWith lost as 7 tries:")
+# for item in data.items():
+#     user = item[1] 
+#     res.append({"res" : user[4]/user[3], "name" : user[2], "solved" : user[3]})
+# res.sort(key=lambda x: x["res"])
+# for each in res:
+#     print(f'{each["name"]:10} ==> average: {each["res"]:.4f} ==> solved: {each["solved"]}')
+
+res = []
+print(f'\nWith unsolved for all days as 7 tries. Maximum solved {max_solved} by {", ".join(solvers)}:')
+for item in data.items():
+    user = item[1]
+    res.append({"res" : (user[1] + 7 * (max_solved - user[0]))/max_solved, "name" : user[2]})
+res.sort(key=lambda x: x["res"])
+for each in res:
+    print(f'{each["name"]:10} ==> average: {each["res"]:.4f}')
   
 # Closing file
 f.close()
